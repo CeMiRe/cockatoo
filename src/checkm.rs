f57af66413ae795773aca0accf48fd776e33ab30
Ben J Woodcroft <donttrustben@gmail.com>
2020-01-13 14:46:28 +1000

    checkm: Add read_file_path method.

--- src/checkm.rs
+++ src/checkm.rs
@@ -1,4 +1,5 @@
 use std;
+use std::collections::BTreeMap;
 use csv;
 use log::*;
 
@@ -7,13 +8,26 @@ pub struct CheckMTabTable {
 
 impl CheckMTabTable {
     pub fn good_quality_genome_names(file_path: &str, min_completeness: f32, max_contamination: f32) -> Vec<String> {
+        let mut passes = vec![];
+        let qualities = CheckMTabTable::read_file_path(file_path);
+        for (genome, quality) in qualities.genome_to_quality.iter() {
+            if quality.completeness >= min_completeness && quality.contamination <= max_contamination {
+                passes.push(genome.clone())
+            }
+        }
+        debug!("Read in {} genomes from {}, {} passed the quality thresholds", 
+            qualities.genome_to_quality.len(), file_path, passes.len());
+        return passes;
+    }
+
+    pub fn read_file_path(file_path: &str) -> CheckMResult {
+        let mut qualities = BTreeMap::new();
         let rdr = csv::ReaderBuilder::new()
             .delimiter(b'\t')
             .has_headers(true)
             .from_path(std::path::Path::new(file_path));
-
-        let mut passes = vec![];
         let mut total_seen = 0usize;
+
         for result in rdr
             .expect(&format!("Failed to parse CheckM tab table {}", file_path))
             .records() {
@@ -26,14 +40,34 @@ impl CheckMTabTable {
             let completeness: f32 = res[11].parse::<f32>().expect("Error parsing completeness in checkm tab table");
             let contamination: f32 = res[12].parse::<f32>().expect("Error parsing contamination in checkm tab table");
             trace!("For {}, found completeness {} and contamination {}", &res[0], completeness, contamination);
-            if completeness >= min_completeness && contamination <= max_contamination {
-                passes.push(res[0].to_string());
-            }
+            match qualities.insert(
+                res[0].to_string(),
+                GenomeQuality { 
+                    completeness: completeness,
+                    contamination: contamination,
+                }) {
+                None => {},
+                Some(_) => {
+                    error!("The genome {} was found multiple times in the checkm file {}", res[0].to_string(), file_path);
+                    std::process::exit(1);
+                }
+            };
             total_seen += 1;
         }
-        debug!("Read in {} genomes from {}, {} passed the quality thresholds", total_seen, file_path, passes.len());
-        return passes;
+        debug!("Read in {} genomes from {}", total_seen, file_path);
+        return CheckMResult {
+            genome_to_quality: qualities
+        };
     }
+}
+
+pub struct CheckMResult {
+    pub genome_to_quality: BTreeMap<String,GenomeQuality>,
+}
+
+pub struct GenomeQuality {
+    pub completeness: f32,
+    pub contamination: f32,
 }
 
 #[cfg(test)]
